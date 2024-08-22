c8e2b6b2f07cc7ca00ca5ba88324f9c076bec379
isaeva <ystropilova@yandex.ru>
2024-08-22 13:33:04 +0300

    commit

--- src/app/pages/service/service.component.ts
+++ src/app/pages/service/service.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {AccordionItemComponent} from "../../components/ui/accordion-item/accordion-item.component";
+import {LanguageService} from "../../services/language.service";
 // import itemsData from "../../../assets/api/serviceData.json"
 
 @Component({
@@ -12,7 +13,15 @@ import {AccordionItemComponent} from "../../components/ui/accordion-item/accordi
   styleUrl: './service.component.css'
 })
 export class ServiceComponent {
+  constructor(private languageService : LanguageService) {
+  }
+
   items = ['concept_design', 'class_drawings', 'workshop_design_documentation', '3d_modelling', 'nesting_documentation', 'operational_and_tests_trials_documentation',
     'designer_supervision', 'ship_reclassification', 'ship_upgrading_reequipment']
   // items = itemsData;
+
+  t(str : string) {
+    return this.languageService.t(str);
+  }
 }
+
