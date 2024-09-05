e3580db08674f7c5ed7e9b719356c03d09268558
dulanova <dinara-dulanova@yandex.ru>
2024-09-05 11:26:29 +0300

    update: accordion-item.component.html

--- src/app/components/ui/accordion-item/accordion-item.component.ts
+++ src/app/components/ui/accordion-item/accordion-item.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
 import {NgClass} from "@angular/common";
+// import * as bootstrap from 'bootstrap';
 import { TranslocoService, TranslocoPipe, TranslocoDirective } from '@jsverse/transloco';
+import {AccordionModule} from "primeng/accordion";
 
 @Component({
   selector: 'app-accordion-item',
   standalone: true,
-  imports: [NgClass, TranslocoPipe, TranslocoDirective],
+  imports: [NgClass, TranslocoPipe, TranslocoDirective, AccordionModule],
   templateUrl: './accordion-item.component.html',
   styleUrl: './accordion-item.component.css'
 })
@@ -13,6 +15,12 @@ export class AccordionItemComponent implements OnInit {
   @Input() title: string | undefined;
 
   expanded: boolean = false;
+
+  content = [];
+  contentClick(content: string): void{
+    // @ts-ignore
+    this.content.includes(content) ? this.content.splice(this.content.indexOf(content), 1) : this.content.push(content);
+  }
 
 
   ngOnInit() {
