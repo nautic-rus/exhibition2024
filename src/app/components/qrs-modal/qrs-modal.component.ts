5b3a5eba913fd7926788e8bac648b622c18d4dfe
isaeva <ystropilova@yandex.ru>
2024-09-09 09:27:47 +0300

    commit

--- src/app/components/qrs-modal/qrs-modal.component.ts
+++ src/app/components/qrs-modal/qrs-modal.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {DynamicDialogConfig, DynamicDialogRef} from "primeng/dynamicdialog";
 import {DomSanitizer} from "@angular/platform-browser";
 import {QRCodeModule} from "angularx-qrcode";
+import {LanguageService} from "../../services/language.service";
 
 @Component({
   selector: 'app-qrs-modal',
@@ -15,7 +16,7 @@ import {QRCodeModule} from "angularx-qrcode";
 export class QrsModalComponent {
   qrRus: string = "";
   qrEng: string = "";
-  constructor(public conf: DynamicDialogConfig, public ref: DynamicDialogRef, private sanitizer: DomSanitizer) {
+  constructor(public conf: DynamicDialogConfig, public ref: DynamicDialogRef, private sanitizer: DomSanitizer, private languageService : LanguageService) {
     this.qrRus = this.conf.data.qrRus;
     this.qrEng = this.conf.data.qrEng;
     console.log(this.conf.data)
@@ -23,5 +24,9 @@ export class QrsModalComponent {
 
   closeModal() {
     this.ref.close();
+  }
+
+  t(str : string) {
+    return this.languageService.t(str);
   }
 }
